4ede8dc3ac1a50b5d3aa3fe4375fc8f45102ae8d
Marcio-Souza <wache2k@gmail.com>
2020-04-21 19:12:36 -0300

    Exercicio para contagem de caracteres

---
+++ contagem_caracteres_lista.py
@@ -0,0 +1,29 @@
+def contar_caracteres(s):
+    """ Funcao que conta os caracteres de uma string
+    Ex:
+    >>> contar_caracteres('banana')
+    a:3
+    b:1
+    n:2
+    :param s:
+    :return:
+    """
+    caracter_ordenado = sorted(s)
+    caracter_anterior = caracter_ordenado[0]
+    contagem = 1
+
+    for caracter in caracter_ordenado[1:]:
+        if caracter == caracter_anterior:
+            contagem += 1
+        else:
+            print("{}: {}".format(caracter_anterior, contagem))
+            caracter_anterior = caracter
+            contagem = 1
+    print("{}: {}".format(caracter_anterior, contagem))
+
+
+if __name__ == '__main__':
+    contar_caracteres('banana')
+    print()
+    contar_caracteres('Mariana')
+
