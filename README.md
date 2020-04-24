9dff5427c264baf2e6c33b585b73afbd00d1074f
Marcio-escritorio <wache2k@gmail.com>
2020-04-24 11:54:50 -0300

    contagem de caracteres com dicionario simplificada

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Repositório de exercícios python utilizados no aprendizado da linguagem.
 
 1 - Contagem de caractere usando lista
+2 - Contagem de caracteres usando dicionário
+3 - Contagem de caracteres usando dicionário simplificada
---
+++ contagem_caracteres_dicionario_melhorada.py
@@ -0,0 +1,19 @@
+def contar_caracteres(s):
+    """ Funcao que conta os caracteres de uma string
+    Ex:
+    >>> contar_caracteres('banana')
+    {'a': 3, 'b': 1, 'n': 2}
+    :param s:
+    :return:
+    """
+    resultado = {}
+
+    for caracter in s:
+        resultado[caracter] = resultado.get(caracter, 0) + 1
+    return resultado
+
+
+if __name__ == '__main__':
+    print(contar_caracteres('banana'))
+    print()
+    print(contar_caracteres('Mariana'))
