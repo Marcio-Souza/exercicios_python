4d8066c2fe21ccacec3a9007b3e5aa39b387dcb7
Marcio-escritorio <wache2k@gmail.com>
2020-08-03 12:51:40 -0300

    Atualizado

--- sorteio_alunos.py
+++ sorteio_alunos.py
@@ -1,21 +1,9 @@
-"""
-    Sorteio de alunos.
-    Recebe os inputs de nomes de alunos e faz um sorteio
-    Ex. 
-    >>>Digite o nome do Aluno: João
-    >>>Digite o nome do Aluno: Maria
-    >>>Digite o nome do Aluno: Antônio
-    >>>Digite o nome do Aluno: Pedro
-    >>>Aluno sorteado: Antônio
-
-"""
-from random import randint
+from random import randrange
 
 alunos = []
 
-for n in range(0,4):
-    alunos.append(input("Digite o nome do aluno: "))
+for n in range(4):
+    alunos.append(input("Digite nome do Aluno: "))
 
-sorteio = randint(0,4)
-
+sorteio = randrange(0, 4)
 print(f'Aluno sorteado: {alunos[sorteio]}')
