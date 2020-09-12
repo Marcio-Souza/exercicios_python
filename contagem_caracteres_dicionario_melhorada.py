1b78e5100ef518320f543423a548c6de3e0a5b23
marcio <wache2k@gmail.com>
2020-09-12 17:38:51 -0300

    Envio de arquivos.

--- contagem_caracteres_dicionario_melhorada.py
+++ contagem_caracteres_dicionario_melhorada.py
@@ -3,10 +3,12 @@ def contar_caracteres(s):
     Ex:
     >>> contar_caracteres('banana')
     {'a': 3, 'b': 1, 'n': 2}
+
     :param s:
     :return:
     """
     resultado = {}
+    s = sorted(s)
 
     for caracter in s:
         resultado[caracter] = resultado.get(caracter, 0) + 1
